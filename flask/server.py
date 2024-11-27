39fc6705d9d40770c8ef072faf97265a23741fd5
platakism <platakism11@gmail.com>
2024-11-27 14:24:01 +0100

    new

--- flask/server.py
+++ flask/server.py
@@ -26,24 +26,24 @@ ERROR_RESPONSE = json.dumps({
 #     except:
 #         return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
 
-@app.route("/", methods = ['GET'])
-def index():
-    try:
-        response =  {
-            "content": "This is the index json response"
-        }
-        return jsonify(response)
-    except:
-        return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
+# @app.route("/", methods = ['GET'])
+# def index():
+#     try:
+#         response =  {
+#             "content": "This is the index json response"
+#         }
+#         return jsonify(response)
+#     except:
+#         return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
 
 #Example method for getting data from the server
-@app.route("/api/f1-race-results",methods=['GET'])
-def getData():
-    try:
-        response = service.getSessionData(int(request.args["year"]), request.args["circuit"],request.args["session"])
-        return jsonify(response.to_json(orient = "records"))
-    except:
-        return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
+# @app.route("/api/f1-race-results",methods=['GET'])
+# def getData():
+#     try:
+#         response = service.getSessionData(int(request.args["year"]), request.args["circuit"],request.args["session"])
+#         return jsonify(response.to_json(orient = "records"))
+#     except:
+#         return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
 
     
 @app.route('/api/f1-standings', methods=['GET'])
@@ -73,8 +73,52 @@ def get_circuits():
     circuits = service.getCircuitsByYear(year)
     return jsonify({"circuits": circuits})
     
+
+@app.route("/", methods = ['GET'])
+def index():
+    try:
+        response =  {
+            "content": "This is the index json response"
+        }
+        return jsonify(response)
+    except:
+        return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
+
+#Example method for getting data from the server
+@app.route("/api/f1-race-results",methods=['GET'])
+def getData():
+    try:
+        response = service.getSessionData(int(request.args["year"]), request.args["circuit"],request.args["session"])
+        return jsonify(response.to_json(orient = "records"))
+    except:
+        return Response(ERROR_RESPONSE, status=500, mimetype='application/json')
+
+@app.route("/api/calendar-year-races", methods=['GET'])
+def getRaces():
+    try:
+        response = service.getRacesForYear(int(request.args.get("year")))
+        return jsonify(response)
+    except:
+        return Response(ERROR_RESPONSE, status=500, mimetype="application/json")
+
+@app.route("/api/pilots-for-race", methods=['GET'])
+def getDrivers():
+    try:
+        response = service.getDriversForRace(int(request.args.get("year")), int(request.args.get("race")))
+        return jsonify(response)
+    except:
+        return Response(ERROR_RESPONSE, status=500, mimetype="application/json")
+
+@app.route("/api/pilots-times-for-race", methods=['GET'])
+def getTimes():
+    try:
+        response = service.getRaceLapTimesForDrivers(int(request.args.get("year")), int(request.args.get("race")), request.args.get("racer1"), request.args.get("racer2"))
+        return jsonify(response)
+    except:
+        return Response(ERROR_RESPONSE, status=500, mimetype="application/json")
+
 if __name__ == '__main__':
     if os.environ.get("ENVIRONMENT") == "DEPLOYMENT":
-        app.run(host='0.0.0.0',port=4000)
+        app.run(host='0.0.0.0',port=5000)
     else:
         app.run(debug=True)
